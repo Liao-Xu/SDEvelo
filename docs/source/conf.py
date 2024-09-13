119f70d7b5e58a7deca603e22d47db46b045dd2d
Xu Liao <xu.smooth.liao@gmail.com>
2024-09-13 11:52:27 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,13 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('..'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SDEvelo'
 copyright = '2024, Xu Liao'
@@ -15,7 +12,6 @@ author = 'Xu Liao'
 release = 'https://pypi.org/project/sdevelo/'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -24,14 +20,21 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-# import os
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**/.ipynb_checkpoints', '**/*-checkpoint.rst']
 
-# # Exclude any file or directory starting with a dot
-# exclude_patterns += [f for f in os.listdir('.') if f.startswith('.')]
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    '.ipynb_checkpoints',
+    '**/.ipynb_checkpoints',
+    '**/.ipynb_checkpoints/**',
+    '*-checkpoint.rst',
+    '**/*-checkpoint.rst',
+    '.*',
+    '**/.*',
+]
 
-# Configuration file for the Sphinx documentation builder.
 # -- Options for HTML output -------------------------------------------------
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
